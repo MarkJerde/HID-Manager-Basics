a90c533166d4bc4f4d57b291e3a28a7f1ec688f3
Mark Jerde <mark.a.jerde@gmail.com>
2020-09-13 22:48:15 -0500

    Add Xcode template

---
+++ HID_Manager_Basics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HID_Manager_Basics
+//
+//  Created by Mark Jerde on 9/13/20.
+//  Copyright © 2020 Jerde Apps LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ HID_Manager_Basics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HID_Manager_Basics
+//
+//  Created by Mark Jerde on 9/13/20.
+//  Copyright © 2020 Jerde Apps LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
